ef9d808b6eea2cc991a3ecf332f99820ddff6b1a
xavinash <z_xavi@163.com>
2023-08-14 22:31:39 +0800

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,16 @@
+str1 = input("please input the string:")
+
+def removeDuplicates(s):
+	flag = 1
+	while flag:
+		original_l = len(s)
+		i = 0
+		while i < len(s)-1:
+			if i>=0 and s[i]==s[i+1]:
+				s=s[:i]+s[i+3:]
+				print(s)
+				i-=2
+			i+=1
+		if original_l == len(s):
+			return s
+removeDuplicates(str1)
